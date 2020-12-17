fb53f5fb3e0b08a81a830f33e782b4f918bb907d
Ertan Kara <kara.ertan9@gmail.com>
2020-12-18 00:42:38 +0300

    Add models / initilaizor and dropping fns

--- db.py
+++ db.py
@@ -7,3 +7,20 @@ connection.row_factory = dict_factory
 
 def close_connection():
     connection.close()
+
+
+def provide_cursor(fn):
+    cursor = connection.cursor()
+
+    def wrapper(*args, **kwargs):
+        err = fn(cursor, *args, **kwargs)
+        if err:
+            print('execs this line', err)
+            connection.rollback()
+            cursor.close()
+            return
+
+        connection.commit()
+        cursor.close()
+
+    return wrapper
--- flags.py
+++ flags.py
@@ -1,3 +1,6 @@
+import db
+
+
 class Action:
     def __init__(self, fn, description):
         self.fn = fn
@@ -8,24 +11,71 @@ flags = {
     '--help': 'help',
     '-h': 'help',
     '--time': 'add_time',
-    '-t': 'add_time'
+    '-t': 'add_time',
+    '-i': 'init_db',
+    '--init': 'init_db',
+    '--drop': 'drop_db'
 }
 
 flag_keys = flags.keys()
 
 
-def get_corresponding_flag_action(given_flag):
-    if given_flag in flag_keys:
-        target_flag_action = flags[given_flag]
-        flags_to_actions[target_flag_action].fn(given_flag)
+def get_corresponding_flag_action(raw_flag: str):
+    parsed_flag = raw_flag.split('=')[0]
+    if parsed_flag in flag_keys:
+        target_flag_action = flags[parsed_flag]
+        flags_to_actions[target_flag_action].fn(raw_flag)
     else:
-        print(f'Unknown flag was received {given_flag}')
+        print(f'Unknown flag was received {parsed_flag}')
 
 
 def display_help_msg(_):
     print('Helper flag recieved')
 
 
+@db.provide_cursor
+def register_time(cursor, given_flag):
+    err = False
+
+    try:
+        pass
+    except:
+        err = True
+
+    return err
+
+
+@db.provide_cursor
+def init_db(cursor, _):
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS projects (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT
+        );
+    ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS hours (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            amount INTEGER NOT NULL,
+            date TEXT
+            project_id REFERENCES projects (id)
+        );
+    ''')
+
+
+@db.provide_cursor
+def drop_db(cursor, _):
+    cursor.execute('''
+        DROP TABLE IF EXISTS projects
+    ''')
+    cursor.execute('''
+        DROP TABLE IF EXISTS hours
+    ''')
+
+
 flags_to_actions = {
-    'help': Action(display_help_msg, 'Display available helper methods')
+    'help': Action(display_help_msg, 'Display available helper methods'),
+    'add_time': Action(register_time, 'Registers time as "worked hour"'),
+    'init_db': Action(init_db, 'Creates necessary models to track work'),
+    'drop_db': Action(drop_db, '__DANGEROUS__ Drops worked hour records not undoable')
 }
